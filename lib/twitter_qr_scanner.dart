ae6bcae2d56380b5d883c33a6e10a994c7d89d5a
Dan Gerchcovich <dan.developer789@gmail.com>
2020-12-24 14:53:07 +1100

    Removed Example Folder

--- lib/twitter_qr_scanner.dart
+++ lib/twitter_qr_scanner.dart
@@ -15,23 +15,17 @@ import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
-typedef void QRViewCreatedCallback(QRViewController controller);
-
 class QRView extends StatefulWidget {
   const QRView({
     @required Key key,
-    @required this.onQRViewCreated,
     @required this.data,
     this.overlay,
-    this.qrCodeBackgroundColor = Colors.blue,
+    this.qrCodeBackgroundColor = Colors.red,
     this.qrCodeForegroundColor = Colors.white,
     this.switchButtonColor = Colors.white,
   })  : assert(key != null),
-        assert(onQRViewCreated != null),
         assert(data != null),
         super(key: key);
-
-  final QRViewCreatedCallback onQRViewCreated;
 
   final ShapeBorder overlay;
   final String data;
@@ -220,82 +214,5 @@ class _QRViewState extends State<QRView> {
 
   Widget _getPlatformQrView() {
     return QrcodeReaderView(key: qrViewKey, onScan: onScan);
-  }
-
-  void _onPlatformViewCreated(int id) async {
-    if (widget.onQRViewCreated == null) {
-      return;
-    }
-    widget.onQRViewCreated(QRViewController._(id, widget.key));
-  }
-}
-
-class _CreationParams {
-  _CreationParams({this.width, this.height});
-
-  static _CreationParams fromWidget(double width, double height) {
-    return _CreationParams(
-      width: width,
-      height: height,
-    );
-  }
-
-  final double width;
-  final double height;
-
-  Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'width': width,
-      'height': height,
-    };
-  }
-}
-
-class QRViewController {
-  static const scanMethodCall = "onRecognizeQR";
-
-  final MethodChannel _channel;
-
-  StreamController<String> _scanUpdateController = StreamController<String>();
-
-  Stream<String> get scannedDataStream => _scanUpdateController.stream;
-
-  QRViewController._(int id, GlobalKey qrKey)
-      : _channel = MethodChannel('com.anka.twitter_qr_scanner/qrview_$id') {
-    if (defaultTargetPlatform == TargetPlatform.iOS) {
-      final RenderBox renderBox = qrKey.currentContext.findRenderObject();
-      _channel.invokeMethod("setDimensions",
-          {"width": renderBox.size.width, "height": renderBox.size.height});
-    }
-    _channel.setMethodCallHandler(
-      (MethodCall call) async {
-        switch (call.method) {
-          case scanMethodCall:
-            if (call.arguments != null) {
-              _scanUpdateController.sink.add(call.arguments.toString());
-            }
-        }
-      },
-    );
-  }
-
-  void flipCamera() {
-    _channel.invokeMethod("flipCamera");
-  }
-
-  void toggleFlash() {
-    _channel.invokeMethod("toggleFlash");
-  }
-
-  void pauseCamera() {
-    _channel.invokeMethod("pauseCamera");
-  }
-
-  void resumeCamera() {
-    _channel.invokeMethod("resumeCamera");
-  }
-
-  void dispose() {
-    _scanUpdateController.close();
   }
 }
